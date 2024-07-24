4bfa537ccba601c25eb5139a9b1a07f3a69bbb77
Harry Pierson <harry.pierson@dbos.dev>
2024-07-24 15:04:17 -0700

    add wagmi/cli

---
+++ wagmi.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@wagmi/cli'
+import { hardhat } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+  out: './abis.ts',
+  contracts: [],
+  plugins: [hardhat({
+    project: '.',
+  })],
+})
